7340171624c180221cc2e1f7d80ceb3cf303761f
makrotheeast <makrotheeast@gmail.com>
2023-01-12 09:05:37 +0300

    Made working prototype of powerset

--- powerset/powerset.py
+++ powerset/powerset.py
@@ -1,94 +1,52 @@
-class HashTable:
-    def __init__(self, sz, stp):
-        if sz <= 0:
-            return None
-        if stp <= 0:
-            return None
-        self.length = sz
-        self.step = stp
-        self.slots = [None] * self.length
+import sys
+
+class PowerSet():
+    def __init__(self):
+        self._size = 100009
+        self.slots = [[None] for _ in range(self._size)]
 
     def hash_fun(self, value):
         id = 0
         for char in value:
             id += ord(char)
-        return id % self.length
-
-    def seek_slot(self, value):
-        index = self.hash_fun(value)
-        visited_slots = []
-        while self.slots[index]:
-            if index in visited_slots:
-                return None
-            visited_slots.append(index)
-            index = (index + self.step) % self.length
-        return index
-
-    def put(self, value):
-        founded_slot = self.seek_slot(value)
-        if founded_slot is None:
-            return None
-        self.slots[founded_slot] = value
-        return founded_slot
-
-    def find(self, value):
-        supposed_slot = self.hash_fun(value)
-        if self.slots[supposed_slot] == value:
-            return supposed_slot
-        curr_slot = (supposed_slot + self.step) % self.length
-        while curr_slot != supposed_slot:
-            if self.slots[curr_slot] == value:
-                return curr_slot
-            curr_slot = (curr_slot + self.step) % self.length
-        return None
-
-
-class PowerSet(HashTable):
-    # I inherit the HashTable class this way because of the
-    # implementation of the check server
-    def __init__(self):
-        self.multiplier = 2
-        # 211 because of its magical meaning. Joke. You can smile
-        # after all. 29 is magic too
-        super().__init__(2011, 5)
+        return id % self._size
 
     def _get_all(self):
-        return [value for value in self.slots if value]
-
-    def seek_slot(self, value):
-        """Looks for an index that can be used to store a value in self.slots.
-        Returns suitable index out range of self.slots."""
-        index = self.hash_fun(value)
-        while index < len(self.slots):
-            if not self.slots[index]:
-                return index
-            last = index
-            index += self.step
-        return index
+        values = []
+        size = 0
+        for slot in self.slots:
+            for value in slot:
+                if value is not None:
+                    values.append(value)
+                    size += 1
+        return {'values' : values, 'size' : size}
 
     def put(self, value):
-        """Puts value into set if it still not there, otherwise
-        do nothing."""
         if self.get(value):
             return
-        founded = self.seek_slot(value)
-        if  founded >= len(self.slots):
-            self.slots.extend([None] * int(len(self.slots) * 0.3))
-        self.slots[founded] = value
+        
+        slot = self.hash_fun(value)
+        
+        if self.slots[slot][0] is None:
+            self.slots[slot][0] = value
+            return
+        self.slots[slot].append(value)
 
     def get(self, value):
-        """Returns True if the value is stored in self.slots,
-        otherwise False."""
-        return value in self.slots
+        slot = self.hash_fun(value)
+        return value in self.slots[slot]
 
     def size(self):
-        return len([x for x in self.slots if x])
-        
+        return self._get_all()['size']
 
     def remove(self, value):
-        # возвращает True если value удалено
-        # иначе False
-        return False
+        if not self.get(value):
+            return False
+        
+        slot = self.hash_fun(value)
+
+        self.slots[slot].remove(value)
+        return True
 
     def intersection(self, set2):
         # пересечение текущего множества и set2
--- powerset/test_powerset.py
+++ powerset/test_powerset.py
@@ -4,47 +4,42 @@ import random
 import string
 
 
-# start = time.time()
-# for i in range(20):
-#     pwset.put('value')
-# end = time.time()
-# print('Время выполнения: {:.3f}'.format(end - start))
-
-
 # Test put
 def test_put_1():
     pwset = PowerSet()
     pwset.put('1')
-    assert pwset._get_all() == ['1']
+    assert pwset._get_all()['values'] == ['1']
 
 
-def test_put_2():
+def test_put_3():
     pwset = PowerSet()
-
-    start = time.time()
-    for i in range(200000):
+    for i in range(20000):
         pwset.put(str(i))
-    end = time.time()
-    print(f'Время добавления = {end - start}')
-    start = time.time()
-    size = pwset.size()
-    end = time.time()
-    assert size == 200000
-    print(f'Время вычисления числа значений = {end - start}')
-    start = time.time()
-    length = len(pwset.slots)
-    end = time.time()
-    print(f'Время вычисления числа слотов = {end - start}')
-
-    print(f'Число элементов = {size}, а слотов = {length}')
+    for i in range(20000):
+        pwset.put(str(i))
+    assert pwset.size() == 20000
 
 
 def test_put_dublicates():
     pwset = PowerSet()
     pwset.put('1')
     assert pwset.get('1')
-    pwset.put('1')    
+    pwset.put('1')
     assert pwset.size() == 1
 
 
-test_put_2()
+# Test remove
+def test_remove_1():
+    pwset = PowerSet()
+    for i in range(20000):
+        pwset.put(str(i))
+    for i in range(20000):
+        assert pwset.remove(str(i)) is True
+    assert pwset.size() == 0
+
+
+def test_remove_2():
+    pwset = PowerSet()
+    pwset.put('1')
+    assert pwset.remove('2') is False
+    assert pwset.size() == 1
