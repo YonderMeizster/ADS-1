53924e7671495afd22531c8ade015e8fdc7d8ba0
makrotheeast <makrotheeast@gmail.com>
2023-01-17 00:43:15 +0300

    Refactored class, added tests

--- nativecahce/nativecahce.py
+++ nativecahce/nativecahce.py
@@ -20,6 +20,25 @@ class NativeCache:
     def _clean_hits(self, index):
         self.hits[index] = 0
 
+    def _check_slots(self, index, condition,
+                     action=lambda value: value):
+        if condition(index):
+            return index
+
+        start = index
+        index = (index + self.step) % self.size
+
+        while index != start:
+            if condition(index):
+                return index
+            index = (index + self.step) % self.size
+
+    def hash_fun(self, value):
+        hash_sum = 0
+        for char in str(value):
+            hash_sum = ((hash_sum * 17) + ord(char)) % self.size
+        return hash_sum
+
     def keys(self):
         _keys = []
         for key in self.slots:
@@ -27,57 +46,47 @@ class NativeCache:
                 _keys.append(key)
         return _keys
 
-    
-    def hash_fun(self, value):
-        hash_sum = 0
-        for char in value:
-            hash_sum = ((hash_sum * 17) + ord(char)) % self.size
-        return hash_sum
-
     def clean_slot(self, index):
         min_hits = self.hits[index]
         min_hits_index = index
-        
+
+        start = index
         index = (index + self.step) % self.size
-        curr = index
-        while curr != index:
-            if self.hits[curr] < min_hits:
-                min_hits = self.hits[curr]
-                min_hits_index = curr
-            curr = (curr + self.step) % self.size
+
+        while index != start:
+            if self.hits[index] < min_hits:
+                min_hits = self.hits[index]
+                min_hits_index = index
+            index = (index + self.step) % self.size
         self._remove(min_hits_index)
         return min_hits_index
 
-    def get_the_slot(self, key, value):
-        index = self.hash_fun(key)
-        if self.slots[index] is None:
-            return index
-        if self.slots[index] == key:
-                return
-        start = index
-        index = (index + self.step) % self.size
-        while index != start:
-            if self.slots[index] is None:
-                return index
-            if self.slots[index] == key:
-                return
-            index = (index + self.step) % self.size
+    def get_slot(self, key, value):
+        index = self.hash_fun(str(key))
+        def condition(index): return self.slots[index] is None
+
+        produced_index = self._check_slots(index, condition)
+        if isinstance(produced_index, int):
+            return produced_index
+
         return self.clean_slot(index)
 
     def put(self, key, value):
+        key = str(key)
         prev_index = self.find(key)
+
         if isinstance(prev_index, int):
             self.slots[prev_index] = key
             self.values[prev_index] = value
             self._clean_hits(prev_index)
             return
-        index = self.get_the_slot(key, value)
-        if index is None:
-            return
+
+        index = self.get_slot(key, value)
         self.slots[index] = key
         self.values[index] = value
 
     def get(self, key):
+        key = str(key)
         index = self.find(key)
         if index is None:
             return None
@@ -85,13 +94,7 @@ class NativeCache:
         return self.values[index]
 
     def find(self, key):
+        key = str(key)
         index = self.hash_fun(key)
-        if self.slots[index] == key:
-            return index
-        start = index
-        index = (index + self.step) % self.size
-        while index != start:
-            if self.slots[index] == key:
-                return index
-            index = (index + self.step) % self.size
-        return
+        def condition(index): return self.slots[index] == key
+        return self._check_slots(index, condition)
--- nativecahce/test_nativecahce.py
+++ nativecahce/test_nativecahce.py
@@ -1,5 +1,5 @@
 from nativecahce import NativeCache
-
+from random import randint
 
 # Test put
 def test_put_1():
@@ -37,6 +37,7 @@ def test_put_5():
     ncache.put('key1', 1)
     assert ncache.get('key1') == 1
 
+
 def test_put_6():
     ncache = NativeCache(1)
     ncache.put('key1', 1)
@@ -52,7 +53,7 @@ def test_put_7():
     keys = ['key0', 'key1', 'key2', 'key3', 'key4']
     for index, key in enumerate(keys):
         ncache.put(key, index)
-    
+
     for index, key in enumerate(keys):
         assert ncache.get(key) == index
 
@@ -65,7 +66,7 @@ def test_put_8():
     keys = ['key0', 'key1', 'key2', 'key3', 'key4']
     for index, key in enumerate(keys):
         ncache.put(key, index)
-    
+
     for index in range(1, 5):
         assert ncache.get(keys[index]) == index
 
@@ -81,23 +82,82 @@ def test_put_9():
     ncache.put('key2', 2)
     index_key1 = ncache.find('key1')
     ncache.hits[index_key1] += 1
-    ncache.put()
-    
-    
+    ncache.put('key3', 3)
+    assert 'key1' in ncache.keys()
+    assert 'key3' in ncache.keys()
+
+
+def test_put_10():
+    ncache = NativeCache(2)
+    ncache.put('key1', 1)
+    ncache.put('key2', 2)
+    index_key2 = ncache.find('key2')
+    ncache.hits[index_key2] += 1
+    ncache.put('key3', 3)
+    assert 'key2' in ncache.keys()
+    assert 'key3' in ncache.keys()
+
+
+def test_put_11():
+    ncache = NativeCache()
+
+    def find_same_hash(key):
+        results = []
+        i = int(key) + 1
+        while len(results) < 3:
+            if ncache.hash_fun(key) == ncache.hash_fun(str(i)):
+                results.append(str(i))
+            i += 1
+        return results
+
+    keys = find_same_hash('1')
+    ncache.put('1', 1)
+    for key in keys:
+        ncache.put(key, 1)
+    assert ncache.find(keys[2]) == 64
+
 
 # Test find
 def test_find_1():
-    pass
+    ncache = NativeCache()
+    assert ncache.find('key') is None
 
 
-# ncache = NativeCache(sz = 5)
-# ncache.put('key', 1)
-# ncache.put('key1', 1)
-# ncache.put('key2', 1)
-# ncache.put('key3', 1)
-# ncache.put('key4', 1)
-# ncache.put('key', 1)
-# print(ncache.get('key'))
-# print(ncache.get('key'))
-# print(ncache.get('key'))
-# print(ncache)
+def test_find_2():
+    ncache = NativeCache()
+    ncache.put('key', 1)
+    assert isinstance(ncache.find('key'), int)
+
+
+# Test get
+def test_get_1():
+    ncache = NativeCache(50)
+    for i in range(50):
+        ncache.put(str(i), i)
+
+    for _ in range(100):
+        for i in range(50):
+            assert ncache.get(str(i)) == i
+
+    for hit in ncache.hits:
+        assert hit == 100
+
+
+def test_get_2():
+    ncache = NativeCache(1)
+    assert ncache.get('key') is None
+
+
+# Test clean_slot
+def test_clean_slot_1():
+    ncache = NativeCache(109)
+    for i in range(109):
+        ncache.put(i, i)
+    for i in range(len(ncache.hits)):
+        ncache.hits[i] = 2
+    ncache.hits[0] = 1
+    key = ncache.slots[0]
+    assert key in ncache.keys()
+    ncache.put('abrakadabra', 1)
+    assert ncache.hits[0] == 0
+    assert key not in ncache.keys()
